3f65cdf5ac221095d97b3ef569371976e34234ae
osycon <fredrichawk@gmail.com>
2017-07-02 11:34:12 +0200

    Change images Improved css Add buttons to start and stop

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,10 +3,24 @@ import { images } from '../images.json';
 import './Slider.css';
 
 class Slider extends Component {
+  constructor(props) {
+    super();
+    this.handleStartSlideshow = this.handleStartSlideshow.bind(this);
+    this.handleStopSlideshow = this.handleStopSlideshow.bind(this);
+  }
+  handleStartSlideshow() {}
+  handleStopSlideshow() {}
   render() {
+    const slide = images.map((img, i) => <img key={i} src={img.url} alt={img.alternate} />);
     return (
       <div className="Slider">
-        {images.map((img, i) => <img key={i} src={img} alt="sunshine" />)}
+        <div className="slides">
+          {slide}
+        </div>
+        <div className="btns">
+          <button onClick={this.handleStartSlideshow}>Start</button>
+          <button onClick={this.handleStopSlideshow}>Stop</button>
+        </div>
       </div>
     );
   }
