f68e7b89b41526e6d117b68fc80602b518f71c7b
osycon <fredrichawk@gmail.com>
2017-07-02 13:04:49 +0200

    Add prev and next buttons

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,22 +4,71 @@ import './Slider.css';
 
 class Slider extends Component {
   constructor(props) {
-    super();
+    super(props);
+    this.state = {
+      currentSlide: 0,
+      interval: null,
+      images
+    };
     this.handleStartSlideshow = this.handleStartSlideshow.bind(this);
     this.handleStopSlideshow = this.handleStopSlideshow.bind(this);
+    this.nextSlide = this.nextSlide.bind(this);
+    this.prevSlide = this.prevSlide.bind(this);
+    this.createTimer = this.createTimer.bind(this);
   }
-  handleStartSlideshow() {}
-  handleStopSlideshow() {}
+  nextSlide() {
+    let index = this.state.currentSlide;
+    if (index === 5) {
+      index = 0;
+      this.setState({
+        currentSlide: index
+      });
+      return;
+    }
+    index = index + 1;
+    this.setState({
+      currentSlide: index
+    });
+  }
+  prevSlide() {
+    let index = this.state.currentSlide;
+    if (index === 0) {
+      index = 5;
+      this.setState({
+        currentSlide: index
+      });
+      return;
+    }
+    index = index - 1;
+    this.setState({
+      currentSlide: index
+    });
+  }
+  createTimer() {
+    const interval = setInterval(() => this.nextSlide(), 1000);
+    this.setState({
+      interval: interval
+    });
+  }
+  handleStartSlideshow() {
+    this.createTimer();
+  }
+  handleStopSlideshow() {
+    clearInterval(this.state.interval);
+  }
   render() {
-    const slide = images.map((img, i) => <img key={i} src={img.url} alt={img.alternate} />);
+    const image = this.state.images[this.state.currentSlide];
+    const slide = <img src={image.url} alt={image.alternate} />;
     return (
       <div className="Slider">
         <div className="slides">
           {slide}
         </div>
         <div className="btns">
+          <button onClick={this.prevSlide}>Prev</button>
           <button onClick={this.handleStartSlideshow}>Start</button>
           <button onClick={this.handleStopSlideshow}>Stop</button>
+          <button onClick={this.nextSlide}>Next</button>
         </div>
       </div>
     );
